94b2d60af81a1e6a7549b570027ae99db8a43add
Zhiyu Wang <zhw197@ucsd.edu>
2018-06-07 12:18:33 -0700

    before pull

--- src/main/webapp/test.js
+++ src/main/webapp/test.js
@@ -31,6 +31,7 @@ sessionStorage.setItem("count", 0);
 
 var id = 0;
 
+//test1: insert a number per sec at pos 0
 insertTest = function() {
   count = sessionStorage.getItem("count");
   console.log("count: " + count);
@@ -47,4 +48,45 @@ periodTest = function() {
   id = setInterval(insertTest, 1000);
 };
 
-$("#test").click(periodTest);
+$("#test1").click(periodTest);
+
+/*
+//test2: insert a number per sec at pos 9-0
+insertTest = function(i) {
+  count = sessionStorage.getItem("count");
+  console.log("count: " + count);
+  webPost(true, 9-count, count);
+  count = parseInt(count)+1;
+  if(count == 10)
+  {
+    clearInterval(id);
+  }
+  sessionStorage.setItem("count", count);
+};
+
+periodTest = function() {
+  id = setInterval(insertTest, 1000);
+};
+
+$("#test2").click(periodTest);
+
+//test3: insert-delete pattern test
+insertTest = function(i) {
+  count = sessionStorage.getItem("count");
+  console.log("count: " + count);
+  var payload;
+  if(count % 2 == 0)
+    payload = count;
+  else
+    payload = "Backspace";
+  webPost(true, 1, payload);
+  count = parseInt(count)+1;
+  if(count == 10)
+  {
+    clearInterval(id);
+  }
+  sessionStorage.setItem("count", count);
+};
+
+$("#test3").click(periodTest);
+*/
